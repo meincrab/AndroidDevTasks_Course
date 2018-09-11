50f54addcf74af5ac3a9d61e4341ed5414d00615
meincrab <meincrab@gmail.com>
2018-09-11 14:17:53 +0300

    Exercise 2 Valmis

--- BasicControls/app/src/main/java/com/example/k8684/basiccontrols/LoginActivity.java
+++ BasicControls/app/src/main/java/com/example/k8684/basiccontrols/LoginActivity.java
@@ -2,6 +2,11 @@ package com.example.k8684.basiccontrols;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends Activity {
 
@@ -9,5 +14,31 @@ public class LoginActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        AutoCompleteTextView actv; // add stings to control
+        actv = (AutoCompleteTextView)
+                findViewById(R.id.login);
+        ArrayAdapter<String> aa = new ArrayAdapter<String>(this,
+                android.R.layout.simple_dropdown_item_1line,
+                new String[]
+                        {"Pasi","Juha","Kari","Jouni","Esa","Hannu"});
+        actv.setAdapter(aa);
     }
+
+    EditText loginInput;
+    EditText passwordInput;
+
+    public void loginButtonClicked(View view) {
+        loginInput = (EditText) findViewById(R.id.login);
+        String loginText = loginInput.getText().toString();
+        passwordInput = (EditText) findViewById(R.id.password);
+        String passwordText = passwordInput.getText().toString();
+        String together = loginText + ", " + passwordText;
+        Toast.makeText(getApplicationContext(), together, Toast.LENGTH_SHORT).show();
+
+    }
+
+
+
+
 }
