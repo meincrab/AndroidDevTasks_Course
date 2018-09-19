0450a15e1894f1ee8a2f389d3a601f651246db2a
meincrab <meincrab@gmail.com>
2018-09-19 08:44:42 +0300

    Task 6, in progress

---
+++ Notifications/app/src/main/java/com/example/k8684/notifications/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.k8684.notifications;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    NotificationCompat.Builder notification;
+    private static final int uniqueID = 45612;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        notification = new NotificationCompat.Builder(this);
+        notification.setAutoCancel(true);
+    }
+
+    public void notificationButtonClicked(View view ){
+        System.out.format("Hello World");
+        notification.setSmallIcon(R.drawable.blackberry);
+        notification.setTicker("This is the ticker");
+        notification.setWhen(System.currentTimeMillis());
+        notification.setContentTitle("Here is the title");
+        notification.setContentText("I am the body text of your notification");
+
+        Intent intent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        notification.setContentIntent(pendingIntent);
+
+        NotificationManager nm = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+    }
+
+}
